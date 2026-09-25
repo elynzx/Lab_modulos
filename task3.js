8f50beff609cd911167902bcedf532bb26d6d2a0
Elynzx <rpascual06@outlook.com>
2026-09-25 14:39:15 -0500

    task 3

--- task3.js
+++ task3.js
@@ -1,3 +1,14 @@
-export function calculadoraEdad() {
-
+export function calculadoraEdad(anio, mes, dia) {
+  const fechaActual = new Date();
+  const anioActual = fechaActual.getFullYear();
+  const mesActual = fechaActual.getMonth();
+  const diaActual = fechaActual.getDate();
+  let edad = anioActual - anio;
+  if (mesActual < mes - 1) {
+    edad--;
+  }
+  if (mes - 1 === mesActual && diaActual < dia) {
+    edad--;
+  }
+  return edad;
 }
