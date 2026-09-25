e4e1a6d245ba0138904ef6c598f8ed08eca0bbc0
Elynzx <rpascual06@outlook.com>
2026-09-25 14:18:46 -0500

    task 2

--- task2.js
+++ task2.js
@@ -1,5 +1,7 @@
 export class NombresAmigos {
-  constructor() {
-
+  constructor(nombre1, nombre2, nombre3) {
+    this.nombre1 = nombre1;
+    this.nombre2 = nombre2;
+    this.nombre3 = nombre3;
   }
 }
