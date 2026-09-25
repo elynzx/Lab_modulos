52ceb70dccb5d2a3c7a748b6d117a87df990183e
Elynzx <rpascual06@outlook.com>
2026-09-25 14:44:05 -0500

    task 4

--- task4.js
+++ task4.js
@@ -1,3 +1,24 @@
 export class EdadAmigo {
+  constructor(nombre, anio, mes, dia) {
+    this.nombre = nombre;
+    this.anio = anio;
+    this.mes = mes;
+    this.dia = dia;
+  }
 
+  retornarEdad() {
+    const fechaActual = new Date();
+    const anioActual = fechaActual.getFullYear();
+    const mesActual = fechaActual.getMonth();
+    const diaActual = fechaActual.getDate();
+    
+    let edad = anioActual - this.anio;
+    if (mesActual < this.mes - 1) {
+      edad--;
+    }
+    if (this.mes - 1 === mesActual && diaActual < this.dia) {
+      edad--;
+    }
+    return `¡${this.nombre} tiene ${edad} años hoy!`;
+  }
 }
