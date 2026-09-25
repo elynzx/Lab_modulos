878e7ad31e18be5c959ed54bd1cda095bc085280
Elynzx <rpascual06@outlook.com>
2026-09-25 14:48:55 -0500

    task 5

--- task5.js
+++ task5.js
@@ -1,3 +1,7 @@
-export function rubricaAprobadoReprobado() {
+export function rubricaAprobadoReprobado(puntuacion) {
+  if (puntuacion < 0 || puntuacion > 11) {
+    return "Puntuacion invalida";
+  }
 
+  return puntuacion >= 5 ? "Aprobado" : "Reprobado";
 }
