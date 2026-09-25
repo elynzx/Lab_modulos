dd3e232ad0057af5635bd9d81f419042ebfe5ee7
Elynzx <rpascual06@outlook.com>
2026-09-25 14:58:07 -0500

    task 7

--- task7.js
+++ task7.js
@@ -1,3 +1,11 @@
-export function rubricaPerfecto() {
-
+export function rubricaPerfecto(puntuacion) {
+  if (puntuacion < 0 || puntuacion > 11) {
+    return "Puntuacion invalida";
+  }
+  if (puntuacion == 11) {
+    return "Perfecto";
+  } else if (puntuacion > 8) {
+    return "Excelente";
+  }
+  return puntuacion >= 5 ? "Aprobado" : "Reprobado";
 }
